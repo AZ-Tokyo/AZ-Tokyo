200e971b92745ac15d29439bd7f5d8861f5d8a2a
E233-3639 <ryoogisoe233@gmail.com>
2025-12-05 16:52:38 +0900

    feature: net/http パッケージで実装した HTTP サーバーを Gin を使って再実装 feature:

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello AZ-Tokyo")
-}
-
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run(":8080")
 }
