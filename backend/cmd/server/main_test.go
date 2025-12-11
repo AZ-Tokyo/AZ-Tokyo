857ab9546b428482361e43ea3bacba10a32b3652
E233-3639 <ryoogisoe233@gmail.com>
2025-12-12 00:28:02 +0900

    test: main_test.go に TestGetIndex 関数を追加で実装

--- backend/cmd/server/main_test.go
+++ backend/cmd/server/main_test.go
@@ -26,3 +26,21 @@ func TestGetRoot(test *testing.T) {
 
 	require.Equal(test, "Hello AZ-Tokyo", got["message"])
 }
+
+func TestGetIndex(test *testing.T) {
+	// "go run cmd/server/main.go" で先にサーバーを起動しておく
+	response, err := http.Get("http://localhost:8080/index")
+	require.NoError(test, err)
+	defer response.Body.Close()
+
+	require.Equal(test, http.StatusOK, response.StatusCode)
+
+	body, err := io.ReadAll(response.Body)
+	require.NoError(test, err)
+
+	var got map[string]string
+	err = json.Unmarshal(body, &got)
+	require.NoError(test, err)
+
+	require.Equal(test, "Hello AZ-Tokyo", got["message"])
+}
