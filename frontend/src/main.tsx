05d72b57777e53d3a4bfed0856b63f323d8c79a8
E233-3639 <ryoogisoe233@gmail.com>
2025-12-29 02:51:17 +0900

    feature: main.tsx の Hello World を App コンポーネントに置き換え

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './main.css'
+import App from "./App.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Hello World</h1>
+    <App></App>
   </StrictMode>,
 )
