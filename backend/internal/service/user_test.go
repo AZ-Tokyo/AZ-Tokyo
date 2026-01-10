b687cafef49a626c5653e4ca826d80c8c66418a8
E233-3639 <ryoogisoe233@gmail.com>
2026-01-10 09:03:41 +0900

    test: *userService 型の UpdateRecord メソッドのテストを実装
    ○ userService 構造体のテストに *userService 型の UpdateRecord メソッドのモック関数を実装
    ○ UpdateRecord メソッドの正常時をテストする TestUpdateRecord_Success メソッドを実装
    ○ UpdateRecord メソッドのエラー時をテストする TestUpdateRecord_Error メソッドを実装

--- backend/internal/service/user_test.go
+++ backend/internal/service/user_test.go
@@ -27,6 +27,11 @@ func (m *MockUserRepository) Create(ctx context.Context, user *model.User) error
 	return args.Error(0)
 }
 
+func (m *MockUserRepository) UpdateRecord(ctx context.Context, user model.User) error {
+	args := m.Called(ctx, user)
+	return args.Error(0)
+}
+
 func TestFindAll_Success(t *testing.T) {
 	ctx := context.Background()
 
@@ -79,6 +84,41 @@ func TestCreate_Error(t *testing.T) {
 
 	service := NewUserService(mockRepo)
 	err := service.Create(ctx, user)
+
+	assert.Error(t, err)
+	mockRepo.AssertExpectations(t)
+}
+
+func TestUpdateRecord_Success(t *testing.T) {
+	ctx := context.Background()
+
+	mockRepo := new(MockUserRepository)
+	user1 := &model.User{Name: "Old 42Tokyo"}
+	user2 := *user1
+	user2.Name = "New 42Tokyo"
+
+	mockRepo.On("Create", mock.Anything, user1).Return(nil)
+	mockRepo.On("UpdateRecord", mock.Anything, user2).Return(nil)
+
+	service := NewUserService(mockRepo)
+	err1 := service.Create(ctx, user1)
+	err2 := service.UpdateRecord(ctx, user2)
+
+	assert.NoError(t, err1)
+	assert.NoError(t, err2)
+	assert.NotEqual(t, *user1, user2)
+	mockRepo.AssertExpectations(t)
+}
+
+func TestUpdateRecord_Error(t *testing.T) {
+	ctx := context.Background()
+
+	mockRepo := new(MockUserRepository)
+	user := model.User{Name: "New 42Tokyo"}
+	mockRepo.On("UpdateRecord", mock.Anything, user).Return(errors.New("Not found record"))
+
+	service := NewUserService(mockRepo)
+	err := service.UpdateRecord(ctx, user)
 
 	assert.Error(t, err)
 	mockRepo.AssertExpectations(t)
